7984cdcce678c30627b02cbc48f1c5f13e39792e
Lewis <lewisdavidsonznergi@gmail.com>
2021-07-26 16:50:16 +0100

    Navbar created

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,8 +1,31 @@
 <?php
     if (!isset($navbar_page)) $navbar_page = "";
 ?>
-<nav class="navbar navbar-expand-md navbar-light navbar-outer container-lg">
-    <div class="container-fluid">
+<nav class="navbar navbar-expand-lg">
+    <a class="navbar-brand" href="#">gigawaffle</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <a class="nav-link {{ ($navbar_page == "home") ? 'navigation-link-current-page' : '' }}" href="{{ route("home") }}">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link {{ ($navbar_page == "clients") ? 'navigation-link-current-page' : '' }}" href="{{ route("clients") }}">Clients</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link {{ ($navbar_page == "services") ? 'navigation-link-current-page' : '' }}" href="{{ route("services") }}">Services</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link {{ ($navbar_page == "about") ? 'navigation-link-current-page' : '' }}" href="{{ route("about") }}">About</a>
+        </li>
+    </div>
+  </nav>
+
+    {{-- <div class="container-fluid">
+        
         <ul style="list-style-type: none; padding: 0px; margin: 0px;">
             <li class="nav-item">
                 <a class="nav-link navigation-link {{ ($navbar_page == "home") ? 'navigation-link-current-page' : '' }}" href="{{ route("home") }}">Home</a>
@@ -28,7 +51,7 @@
     
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto" style="display: flex; align-items: center; font-size: 22px;">
-                {{-- Dropdown Example
+                Dropdown Example
                 <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Dropdown
@@ -39,7 +62,7 @@
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item" href="#">Something else here</a>
                 </div>
-                </li> --}}
+                </li>
                 <div class="d-block d-md-none">
                     <ul style="list-style-type: none; padding: 0px; margin: 0px;">
                         <li class="nav-item">
@@ -55,5 +78,4 @@
                 </div>
             </ul>
         </div>
-    </div>
-</nav>
+    </div> --}}
