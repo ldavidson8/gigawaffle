eedf0596066aa074916497b15de0eea39532f2dc
Mark <mark.graham004@gmail.com>
2021-07-26 17:40:55 +0100

    Started the clients page

--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -1,19 +1,27 @@
 @extends('layouts.master')
 
+@section('stylesheets')
+    <style>
+        h1
+        {
+            display: inline-block;
+            border-bottom: 6px solid #000733;
+            color: #000733;
+            font-style: italic;
+        }
+    </style>
+@endsection
+
 @section('before-header')
     <div class="full-size container-fluid d-flex h-100 flex-column">
 @endsection
 
 @section('main-content')
-        <hr />
-        <div class="row flex-grow-1 no-padding background-image-powerline">
-            <div class="container center-content no-margin" style="padding: 30px;">
-                <h1>Clients Who Trust Us</h1>
-                <p>
-                    We provide businesses and people like YOU with the opportunity to save on their energy bills. With our no-nonsense approach, what you see is what you get.
-                    At Swap My Energy, we give you savings based on facts, not estimates.
-                    By finding you a unique deal you will have the opportunity to save money and the peace of mind that your energy bill switching is in good hands.
-                </p>
+        <div class="container">
+            <div class="flex-grow-1 no-padding center-content" style="padding: 30px;">
+                <h1>A new agency that suits you</h1>
+                <p><b>For some people, understanding what is needed to elevate your business can be hard. This is where we come in. Our dedicated team at gigawaffle are ready to turn your business into a brand.</b></p>
+                <p>We're always on the look out for ways we can improve our efficiency, quality, accuracy, and more. This allows us to be on top of the latest design, development and marketing trends. We'll keep you up to date with everything gigawaffle via "The Waffle Corner".</p>
             </div>
         </div>
     </div>
